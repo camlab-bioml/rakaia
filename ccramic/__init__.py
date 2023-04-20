7b4478581f4e4d13f3b7fbc808088ba4aa4565ec
matt-sd-watson <matt.sd.watson@gmail.com>
2023-04-20 13:56:46 -0400

    embed in flask

--- ccramic/__init__.py
+++ ccramic/__init__.py
@@ -1,2 +1,37 @@
 _program = "ccramic"
 __version__ = "0.1.0"
+import ccramic.routes
+from flask import Flask
+from flask_caching import Cache
+from flask import render_template
+
+
+def init_app():
+    """Construct core Flask application with embedded Dash app."""
+    app = Flask(__name__, instance_relative_config=False)
+    # app.config.from_object('config.Config')
+
+    app.cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+    @app.route('/')
+    @app.route('/ccramic')
+    def home():
+        """Landing page."""
+        return render_template(
+            'index.jinja2',
+            title='ccramic',
+            description='Cell-type Classification (using) Rapid Analysis (of) Multiplexed Imaging (mass) Cytometry.',
+            template='home-template',
+            body="This is a homepage served with Flask."
+        )
+
+    with app.app_context():
+        # Import parts of our core Flask app
+        import ccramic.routes
+
+        # Import Dash application
+        from ccramic.app.app import init_dashboard, init_callbacks
+        app = init_dashboard(app)
+        # init_callbacks(app)
+
+        return app
--- tests/test_basic_connection.py
+++ tests/test_basic_connection.py
@@ -6,27 +6,44 @@ import platform
 from dash.testing.application_runners import import_app
 from selenium.common import NoSuchElementException
 import time
+from ccramic import init_app
+from ccramic.app.app import init_dashboard
+import json
 
 
-@pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") != "true" or platform.system() != 'Linux',
-                    reason="Only test the connection in a GA workflow due to passwordless sudo")
-def test_for_connection():
+@pytest.fixture(scope="module")
+def ccramic_flask_test_app():
+    app = init_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(2)
-    Popen(["echo yes| freeport 8050"], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
-    # p.stdin.write("yes\n")
-    # p.communicate(input=b'yes')
-    result = sock.connect_ex(('localhost', 8050))
-    assert result != 0
-    # assert result == 0
-    new_process = Popen(["python", "ccramic/main.py"],
-                        stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
-    result = sock.connect_ex(('localhost', 8050))
-    assert result == 103
-    new_process.kill()
-    result = sock.connect_ex(('localhost', 8050))
-    assert result != 103 and result != 0
+
+@pytest.fixture(scope="module")
+def client(ccramic_flask_test_app):
+    return ccramic_flask_test_app.test_client()
+
+
+# @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") != "true" or platform.system() != 'Linux',
+#                     reason="Only test the connection in a GA workflow due to passwordless sudo")
+# def test_for_connection():
+# 
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     sock.settimeout(2)
+#     Popen(["echo yes| freeport 5000"], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+#     # p.stdin.write("yes\n")
+#     # p.communicate(input=b'yes')
+#     result = sock.connect_ex(('localhost', 5000))
+#     assert result != 0
+#     # assert result == 0
+#     new_process = Popen(["python", "ccramic/main.py"],
+#                         stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+#     result = sock.connect_ex(('localhost', 5000))
+#     assert result == 103
+#     new_process.kill()
+#     result = sock.connect_ex(('localhost', 5000))
+#     assert result != 103 and result != 0
 
 #
 # def test_run_docker_basic():
@@ -54,16 +71,18 @@ def recursive_wait_for_elem(app, elem, duration):
             recursive_wait_for_elem(app, elem, int(1.1*duration))
 
 
-def test_basic_app_load_from_locale(dash_duo):
-    ccramic_app = import_app("ccramic.app.app")
-    assert str(type(ccramic_app)) == "<class 'dash_extensions.enrich.DashProxy'>"
-    dash_duo.start_server(ccramic_app)
-    for elem in ['#upload-image', '#tiff-image-type', '#image_layers', "#images_in_blend"]:
-        assert dash_duo.find_element(elem) is not None
-    with pytest.raises(NoSuchElementException):
-        assert dash_duo.find_element('#fake-input') is not None
-
-    recursive_wait_for_elem(dash_duo, '#tab-quant', 1)
-
-    for elem in ['#upload-quantification']:
-        assert dash_duo.find_element(elem) is not None
+def test_basic_app_load_from_locale(ccramic_flask_test_app, client):
+    assert str(type(ccramic_flask_test_app)) == "<class 'flask.app.Flask'>"
+    response = client.get('/')
+    assert response.status_code == 200
+    # test landing page alias
+    assert client.get("/ccramic").data == response.data
+    # for elem in ['#upload-image', '#tiff-image-type', '#image_layers', "#images_in_blend"]:
+    #     assert dash_duo.find_element(elem) is not None
+    # with pytest.raises(NoSuchElementException):
+    #     assert dash_duo.find_element('#fake-input') is not None
+    #
+    # recursive_wait_for_elem(dash_duo, '#tab-quant', 1)
+    #
+    # for elem in ['#upload-quantification']:
+    #     assert dash_duo.find_element(elem) is not None
