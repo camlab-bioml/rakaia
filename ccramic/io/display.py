ecac524db8fb7d060e0e59198653a52dcfd028a3
matt-sd-watson <matt.sd.watson@gmail.com>
2023-12-05 12:41:53 -0500

    Use region classes

--- ccramic/io/display.py
+++ ccramic/io/display.py
@@ -2,6 +2,7 @@ import math
 from ccramic.utils.pixel_level_utils import get_area_statistics_from_rect, get_area_statistics_from_closed_path
 from numpy.core._exceptions import _ArrayMemoryError
 import pandas as pd
+from ccramic.utils.region import ZoomRegion, RectangleRegion, FreeFormRegion
 
 def generate_area_statistics_dataframe(graph_layout, upload, layers, data_selection, aliases_dict,
                                        zoom_keys = ['xaxis.range[1]', 'xaxis.range[0]',
@@ -26,32 +27,20 @@ def generate_area_statistics_dataframe(graph_layout, upload, layers, data_select
             try:
                 # option 1: if the shape is drawn with a rectangle
                 if shape['type'] == 'rect':
-                    x_range_low = math.ceil(int(shape['x0']))
-                    x_range_high = math.ceil(int(shape['x1']))
-                    y_range_low = math.ceil(int(shape['y0']))
-                    y_range_high = math.ceil(int(shape['y1']))
-
-                    assert x_range_high >= x_range_low
-                    assert y_range_high >= y_range_low
                     for layer in layers:
-                        mean_exp, max_xep, min_exp = get_area_statistics_from_rect(
-                            upload[data_selection][layer],
-                            x_range_low,
-                            x_range_high,
-                            y_range_low, y_range_high)
-                        mean_panel.append(round(float(mean_exp), 2))
-                        max_panel.append(round(float(max_xep), 2))
-                        min_panel.append(round(float(min_exp), 2))
+                        region_shape = RectangleRegion(upload[data_selection][layer], shape)
+                        mean_panel.append(round(float(region_shape.compute_pixel_mean()), 2))
+                        max_panel.append(round(float(region_shape.compute_pixel_max()), 2))
+                        min_panel.append(round(float(region_shape.compute_pixel_min()), 2))
                         aliases.append(aliases_dict[layer] if layer in aliases_dict.keys() else layer)
                         region.append(region_index)
                     # option 2: if a closed form shape is drawn
                 elif shape['type'] == 'path' and 'path' in shape:
                     for layer in layers:
-                        mean_exp, max_xep, min_exp = get_area_statistics_from_closed_path(
-                            upload[data_selection][layer], shape['path'])
-                        mean_panel.append(round(float(mean_exp), 2))
-                        max_panel.append(round(float(max_xep), 2))
-                        min_panel.append(round(float(min_exp), 2))
+                        region_shape = FreeFormRegion(upload[data_selection][layer], shape)
+                        mean_panel.append(round(float(region_shape.compute_pixel_mean()), 2))
+                        max_panel.append(round(float(region_shape.compute_pixel_max()), 2))
+                        min_panel.append(round(float(region_shape.compute_pixel_min()), 2))
                         aliases.append(aliases_dict[layer] if layer in aliases_dict.keys() else layer)
                         region.append(region_index)
                 region_index += 1
@@ -60,7 +49,7 @@ def generate_area_statistics_dataframe(graph_layout, upload, layers, data_select
                 # min_panel.append(round(sum(shapes_min) / len(shapes_min), 2))
 
             except (AssertionError, ValueError, ZeroDivisionError, IndexError, TypeError,
-                    _ArrayMemoryError):
+                    _ArrayMemoryError, KeyError):
                 pass
 
         layer_dict = {'Channel': aliases, 'Mean': mean_panel, 'Max': max_panel, 'Min': min_panel,
@@ -72,26 +61,16 @@ def generate_area_statistics_dataframe(graph_layout, upload, layers, data_select
             all([elem in graph_layout for elem in zoom_keys]):
 
         try:
-            assert all([elem >= 0 for elem in graph_layout.keys() if isinstance(elem, float)])
-            x_range_low = math.ceil(int(graph_layout['xaxis.range[0]']))
-            x_range_high = math.ceil(int(graph_layout['xaxis.range[1]']))
-            y_range_low = math.ceil(int(graph_layout['yaxis.range[1]']))
-            y_range_high = math.ceil(int(graph_layout['yaxis.range[0]']))
-            assert x_range_high >= x_range_low
-            assert y_range_high >= y_range_low
-
             mean_panel = []
             max_panel = []
             min_panel = []
             aliases = []
+
             for layer in layers:
-                mean_exp, max_xep, min_exp = get_area_statistics_from_rect(upload[data_selection][layer],
-                                                                           x_range_low,
-                                                                           x_range_high,
-                                                                           y_range_low, y_range_high)
-                mean_panel.append(round(float(mean_exp), 2))
-                max_panel.append(round(float(max_xep), 2))
-                min_panel.append(round(float(min_exp), 2))
+                region = ZoomRegion(upload[data_selection][layer], graph_layout)
+                mean_panel.append(round(float(region.compute_pixel_mean()), 2))
+                max_panel.append(round(float(region.compute_pixel_max()), 2))
+                min_panel.append(round(float(region.compute_pixel_min()), 2))
                 aliases.append(aliases_dict[layer] if layer in aliases_dict.keys() else layer)
 
             layer_dict = {'Channel': aliases, 'Mean': mean_panel, 'Max': max_panel, 'Min': min_panel}
--- ccramic/utils/region.py
+++ ccramic/utils/region.py
@@ -1,39 +1,96 @@
-from abc import ABC, abstractmethod
+import math
+from ccramic.utils.pixel_level_utils import get_area_statistics_from_rect, get_area_statistics_from_closed_path
 
-
-class ChannelRegion(ABC):
+class ChannelRegion:
     """
     This abstract class defines a region for a particular channel
     """
-    @abstractmethod
-    def __init__(self, channel_array, coordinate_dict):
+    def __init__(self, channel_array, coordinates):
         self.channel_array = channel_array
-        self.coordinate_dict = coordinate_dict
+        self.coordinate_dict = coordinates
+        self.mean_exp = None
+        self.max_exp = None
+        self.min_exp = None
 
-    @abstractmethod
     def compute_pixel_mean(self):
         """
         Compute the mean pixel intensity of the channel region
         """
-        pass
+        return self.mean_exp
 
-    @abstractmethod
     def compute_pixel_min(self):
         """
         Compute the min pixel intensity of the channel region
         """
-        pass
+        return self.min_exp
 
-    @abstractmethod
+
     def compute_pixel_max(self):
         """
         Compute the max pixel intensity of the channel region
         """
-        pass
+        return self.max_exp
 
-class ZoomRegion(ChannelRegion, ABC):
+class ZoomRegion(ChannelRegion):
     """
     This class defines a channel region created using the zoom feature
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, channel_array, coordinates):
+        super().__init__(channel_array, coordinates)
+        self.required_keys = ['xaxis.range[1]', 'xaxis.range[0]', 'yaxis.range[1]', 'yaxis.range[0]']
+        if all([elem in self.coordinate_dict] for elem in self.required_keys):
+            try:
+                assert all([elem >= 0 for elem in self.coordinate_dict.keys() if isinstance(elem, float)])
+                x_range_low = math.ceil(int(self.coordinate_dict['xaxis.range[0]']))
+                x_range_high = math.ceil(int(self.coordinate_dict['xaxis.range[1]']))
+                y_range_low = math.ceil(int(self.coordinate_dict['yaxis.range[1]']))
+                y_range_high = math.ceil(int(self.coordinate_dict['yaxis.range[0]']))
+                assert x_range_high >= x_range_low
+                assert y_range_high >= y_range_low
+
+                self.mean_exp, self.max_exp, self.min_exp = get_area_statistics_from_rect(self.channel_array,
+                                                                                          x_range_low,
+                                                                                          x_range_high,
+                                                                                          y_range_low, y_range_high)
+            except AssertionError:
+                self.mean_exp, self.max_exp, self.min_exp = 0, 0, 0
+
+
+class RectangleRegion(ChannelRegion):
+    """
+    This class defines a channel region created using by drawing a rectangle
+    """
+    def __init__(self, channel_array, coordinates, first_draw=True):
+        super().__init__(channel_array, coordinates)
+        self.required_keys = ('x0', 'x1', 'y0', 'y1') if first_draw else ('shapes[1].x0', 'shapes[1].x1',
+                                                                          'shapes[1].y0', 'shapes[1].y1')
+        if all([elem in self.coordinate_dict] for elem in self.required_keys):
+            try:
+                assert all([elem >= 0 for elem in self.coordinate_dict.keys() if isinstance(elem, float)])
+                x_range_low = math.ceil(int(self.coordinate_dict[self.required_keys[0]]))
+                x_range_high = math.ceil(int(self.coordinate_dict[self.required_keys[1]]))
+                y_range_low = math.ceil(int(self.coordinate_dict[self.required_keys[2]]))
+                y_range_high = math.ceil(int(self.coordinate_dict[self.required_keys[3]]))
+                assert x_range_high >= x_range_low
+                assert y_range_high >= y_range_low
+
+                self.mean_exp, self.max_exp, self.min_exp = get_area_statistics_from_rect(self.channel_array,
+                                                                                          x_range_low,
+                                                                                          x_range_high,
+                                                                                          y_range_low, y_range_high)
+            except AssertionError:
+                self.mean_exp, self.max_exp, self.min_exp = 0, 0, 0
+
+class FreeFormRegion(ChannelRegion):
+    """
+    This class defines a channel region created by drawing a freeform SVG path shape
+    """
+
+    def __init__(self, channel_array, coordinates):
+        super().__init__(channel_array, coordinates)
+        self.path = self.coordinate_dict['path'] if 'path' in self.coordinate_dict else None
+        if self.path is not None:
+            self.mean_exp, self.max_exp, self.min_exp = get_area_statistics_from_closed_path(
+                self.channel_array, self.path)
+        else:
+            self.mean_exp, self.max_exp, self.min_exp = 0, 0, 0
