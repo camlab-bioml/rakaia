6465c09ec409adb41b889bf3ea6cf46af8ee04dd
matt-sd-watson <matt.sd.watson@gmail.com>
2023-12-20 15:45:02 -0500

    modify tmp clear testing

--- tests/test_session_utils.py
+++ tests/test_session_utils.py
@@ -1,16 +1,18 @@
 import os
 from ccramic.utils.session import remove_ccramic_caches
+import tempfile
 
 def test_session_cache_clearing():
     unique_id = "dsfpihdsfpidjfd"
-    if not os.path.isdir(os.path.join('/tmp/', unique_id)):
-        os.mkdir(os.path.join('/tmp/', unique_id))
-    if not os.path.isdir(os.path.join('/tmp/', unique_id, 'ccramic_cache')):
-        os.mkdir(os.path.join('/tmp/', unique_id, 'ccramic_cache'))
-    if not os.path.isdir(os.path.join('/tmp/', unique_id, 'different_directory')):
-        os.mkdir(os.path.join('/tmp/', unique_id, 'different_directory'))
-    assert os.path.isdir(os.path.join('/tmp/', unique_id, 'ccramic_cache'))
-    assert os.path.isdir(os.path.join('/tmp/', unique_id, 'different_directory'))
-    remove_ccramic_caches('/tmp/')
-    assert not os.path.isdir(os.path.join('/tmp/', unique_id, 'ccramic_cache'))
-    # assert os.path.isdir(os.path.join('/tmp/', unique_id, 'different_directory'))
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        if not os.path.isdir(os.path.join(tmpdirname, unique_id)):
+            os.mkdir(os.path.join(tmpdirname, unique_id))
+        if not os.path.isdir(os.path.join(tmpdirname, unique_id, 'ccramic_cache')):
+            os.mkdir(os.path.join(tmpdirname, unique_id, 'ccramic_cache'))
+        if not os.path.isdir(os.path.join(tmpdirname, unique_id, 'different_directory')):
+            os.mkdir(os.path.join(tmpdirname, unique_id, 'different_directory'))
+        assert os.path.isdir(os.path.join(tmpdirname, unique_id, 'ccramic_cache'))
+        assert os.path.isdir(os.path.join(tmpdirname, unique_id, 'different_directory'))
+        remove_ccramic_caches(tmpdirname)
+        assert not os.path.isdir(os.path.join(tmpdirname, unique_id, 'ccramic_cache'))
+        # assert os.path.isdir(os.path.join(tmpdirname, unique_id, 'different_directory'))
