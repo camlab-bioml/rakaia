92955a9d44abbb202edb0beedf584d613f547547
matt-sd-watson <mwatson@lunenfeld.ca>
2026-01-23 13:11:03 -0500

    Chore: add toggle to downsample ROI gallery

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.28.0] - 2026-01-23
+
+### Added
+
+- Toggle for down-sampling ROI gallery thumbnails
+
 ## [0.27.0] - 2026-01-07
 
 ### Added
--- rakaia/utils/alert.py
+++ rakaia/utils/alert.py
@@ -161,7 +161,9 @@ class ToolTips(BaseModel):
                       "wsi-transform": "Select an affine transformation matrix to map the canvas image to the current WSI;"
                                        " if selected, zooming and panning on the blend canvas will apply transformed coordinates to the WSI viewer. "
                                        "Imported matrices are tagged by their import filename; matched names between image and transformation files allow "
-                                       "auto-selection on WSI change."}
+                                       "auto-selection on WSI change.",
+                      "downsample-roi-thumbnail": "Render down-sampled ROIs in the dataset gallery. Down-sampling may be faster "
+                                                  "and could save browser memory at the cost of thumbnail resolution."}
 
 
 class PanelMismatchError(Exception):
